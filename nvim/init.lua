bce12f6dc14a6c0c06668709e1fdc3595d4c4e11
mashaal <omar@teacups.io>
2025-07-15 13:12:38 +1000

    updates

--- nvim/init.lua
+++ nvim/init.lua
@@ -3,22 +3,22 @@
 -- [[ Keymaps ]]
 vim.keymap.set({ "i", "x", "n", "s" }, "<C-s>", "<cmd>w<cr><esc>", { desc = "Save file" })
 vim.keymap.set(
-  { "x", "n", "s" },
-  "<C-e>",
-  "<esc>mz<bar>vip<bar>:w !sh /home/slash/.config/tidal/s_tidal.sh<CR>`z",
-  { desc = "Tidal wave", silent = true, noremap = true }
+	{ "x", "n", "s" },
+	"<C-e>",
+	"<esc>mz<bar>vip<bar>:w !sh /home/slash/.config/tidal/s_tidal.sh<CR>`z",
+	{ desc = "Tidal wave", silent = true, noremap = true }
 )
 vim.keymap.set(
-  { "i" },
-  "<C-e>",
-  "<esc>mz<bar>vip<bar>:w !sh /home/slash/.config/tidal/s_tidal.sh<CR>`zli",
-  { desc = "Tidal wave", silent = true, noremap = true }
+	{ "i" },
+	"<C-e>",
+	"<esc>mz<bar>vip<bar>:w !sh /home/slash/.config/tidal/s_tidal.sh<CR>`zli",
+	{ desc = "Tidal wave", silent = true, noremap = true }
 )
 vim.keymap.set(
-  { "i", "x", "n", "s" },
-  "<C-t>",
-  '<esc>mz<bar>f"<bar>va"<bar>:w !sh ~/.config/tidal/tactus.sh<CR>',
-  { desc = "Tidal Info" }
+	{ "i", "x", "n", "s" },
+	"<C-t>",
+	'<esc>mz<bar>f"<bar>va"<bar>:w !sh ~/.config/tidal/tactus.sh<CR>',
+	{ desc = "Tidal Info" }
 )
 
 -- [[ General Options ]]
@@ -31,7 +31,7 @@ vim.opt.scrolloff = 8
 vim.opt.termguicolors = true
 vim.opt.background = "dark"
 vim.opt.guicursor =
-"n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,i-ci-ve:ver25"
+	"n-v-c-sm-i-ci-ve:block,r-cr-o:hor20,a:blinkwait700-blinkoff400-blinkon250-Cursor/lCursor,i-ci-ve:ver25"
 
 -- Colorscheme & Highlights
 vim.cmd.colorscheme("oxocarbon")
@@ -73,116 +73,122 @@ vim.opt.updatetime = 50
 
 -- [[ Filetype Detection ]]
 vim.filetype.add({
-  filename = {
-    [".tidal"] = "haskell",
-  },
+	filename = {
+		[".tidal"] = "haskell",
+	},
 })
 
 -- [[ Packer Bootstrap ]]
 local ensure_packer = function()
-  local fn = vim.fn
-  local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
-  if fn.empty(fn.glob(install_path)) > 0 then
-    fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
-    vim.cmd([[packadd packer.nvim]])
-    return true
-  end
-  return false
+	local fn = vim.fn
+	local install_path = fn.stdpath("data") .. "/site/pack/packer/start/packer.nvim"
+	if fn.empty(fn.glob(install_path)) > 0 then
+		fn.system({ "git", "clone", "--depth", "1", "https://github.com/wbthomason/packer.nvim", install_path })
+		vim.cmd([[packadd packer.nvim]])
+		return true
+	end
+	return false
 end
 
 local packer_bootstrap = ensure_packer()
 
 -- [[ Plugins ]]
 return require("packer").startup(function(use)
-  use("wbthomason/packer.nvim")
+	use("wbthomason/packer.nvim")
 
-  use("nyoom-engineering/oxocarbon.nvim")
+	use("nyoom-engineering/oxocarbon.nvim")
 
-  use("andweeb/presence.nvim")
-  use({
-    "dcampos/nvim-snippy",
-    config = function()
-      require("snippy").setup({
-        snippet_paths = { "~/.config/nvim/snippets" },
-        mappings = {
-          is = {
-            ["<Tab>"] = "expand_or_advance",
-            ["<S-Tab>"] = "previous",
-          },
-          nx = {
-            ["<leader>x"] = "cut_text",
-          },
-        },
-      })
-    end,
-  })
-  use("mbbill/undotree")
-  -- use("nvim-lua/plenary.nvim")
-  use({
-    "sphamba/smear-cursor.nvim",
-    config = function()
-      require("smear_cursor").enabled = true
-    end,
-  })
-  use("tpope/vim-commentary")
-  use("tpope/vim-fugitive")
+	use("andweeb/presence.nvim")
+	use({
+		"dcampos/nvim-snippy",
+		config = function()
+			require("snippy").setup({
+				snippet_paths = { "~/.config/nvim/snippets" },
+				mappings = {
+					is = {
+						["<Tab>"] = "expand_or_advance",
+						["<S-Tab>"] = "previous",
+					},
+					nx = {
+						["<leader>x"] = "cut_text",
+					},
+				},
+			})
+		end,
+	})
+	use("mbbill/undotree")
+	-- use("nvim-lua/plenary.nvim")
+	use({
+		"sphamba/smear-cursor.nvim",
+		config = function()
+			require("smear_cursor").enabled = true
+		end,
+	})
+	use("tpope/vim-commentary")
+	use("tpope/vim-fugitive")
 
-  use({
-    "junegunn/fzf.vim",
-    requires = { "junegunn/fzf", run = ":call fzf#install()" },
-  })
+	use({
+		"junegunn/fzf.vim",
+		requires = { "junegunn/fzf", run = ":call fzf#install()" },
+		config = function()
+			vim.api.nvim_create_user_command("FF", "Files", {})
+			vim.api.nvim_create_user_command("FB", "Buffers", {})
+			vim.api.nvim_create_user_command("FL", "Lines", {})
+			vim.api.nvim_create_user_command("FR", "Rg", {})
+		end,
+	})
 
-  use({
-    "neovim/nvim-lspconfig",
-    config = function()
-      require("lspconfig").denols.setup({})
-    end,
-  })
-  use({
-    "stevearc/conform.nvim",
-    config = function()
-      require("conform").setup({
-        formatters_by_ft = {
-          lua = { "stylua" },
-          go = { "gofmt" },
-          haskell = { "fourmolu" },
-          javascript = { "deno_fmt" },
-        },
-        format_on_save = {
-          timeout_ms = 500,
-          lsp_format = "fallback",
-        },
-      })
-    end,
-  })
+	use({
+		"neovim/nvim-lspconfig",
+		config = function()
+			require("lspconfig").denols.setup({})
+		end,
+	})
+	use({
+		"stevearc/conform.nvim",
+		config = function()
+			require("conform").setup({
+				formatters_by_ft = {
+					lua = { "stylua" },
+					go = { "gofmt" },
+					haskell = { "fourmolu" },
+					javascript = { "deno_fmt" },
+				},
+				format_on_save = {
+					timeout_ms = 500,
+					lsp_format = "fallback",
+				},
+			})
+		end,
+	})
 
-  use {
-    "nvim-treesitter/nvim-treesitter",
-    run = ":TSUpdate",
-    config = function()
-      require "nvim-treesitter.configs".setup {
-        incremental_selection = {
-          enable = true,
-          keymaps = {
-            node_incremental = "v",
-            node_decremental = "V",
-          },
-        },
-      }
-    end,
-  }
+	use({
+		"nvim-treesitter/nvim-treesitter",
+		run = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				incremental_selection = {
+					enable = true,
+					keymaps = {
+						node_incremental = "v",
+						node_decremental = "V",
+					},
+				},
+			})
+		end,
+	})
 
-  use {
-    "folke/trouble.nvim",
-    requires = "nvim-tree/nvim-web-devicons",
-    config = function()
-      require("trouble").setup {
-        -- your configuration comes here
-      }
-    end
-  }
+	use({
+		"folke/trouble.nvim",
+		requires = "nvim-tree/nvim-web-devicons",
+		config = function()
+			require("trouble").setup({
+				-- your configuration comes here
+			})
+		end,
+	})
 
-  if packer_bootstrap then
-    require("packer").sync()
-  end
+	if packer_bootstrap then
+		require("packer").sync()
+	end
 end)
