7d118b5ba86195733a308f38a013ec6cdd55afc9
Omar Mashaal <omar@teacups.io>
2025-08-17 21:20:31 +1000

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,1 @@
+yay -S hyprland hyprshade nvim zed-preview waybar dunst hyprpaper ranger supercollider tofi zen-browser wezterm-git hyprlock hypridle nicotine signal-desktop kitty pixterm-git chromium
