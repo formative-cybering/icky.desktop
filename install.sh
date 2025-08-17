ada76dd6fdb8af9626ef1530ee5231909a686dc2
Omar Mashaal <omar@teacups.io>
2025-08-17 23:21:21 +1000

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,2 +1,8 @@
+# yay
+sudo pacman -S --needed git base-devel yay
+
+# mostly everything
 yay -S hyprland hyprshade nvim zed-preview waybar dunst hyprpaper ranger supercollider tofi zen-browser-bin wezterm-git hyprlock hypridle nicotine signal-desktop kitty pixterm-git chromium cpio qpwgraph github-cli dolphin lollypop 1password 
 
+# ghc/haskell
+curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | sh
