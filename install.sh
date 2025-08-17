8691a1351bf681ed8588431c7bc91640d9c3467b
Omar Mashaal <omar@teacups.io>
2025-08-17 22:50:25 +1000

    wip

--- install.sh
+++ install.sh
@@ -1,1 +1,2 @@
-yay -S hyprland hyprshade nvim zed-preview waybar dunst hyprpaper ranger supercollider tofi zen-browser wezterm-git hyprlock hypridle nicotine signal-desktop kitty pixterm-git chromium
+yay -S hyprland hyprshade nvim zed-preview waybar dunst hyprpaper ranger supercollider tofi zen-browser-bin wezterm-git hyprlock hypridle nicotine signal-desktop kitty pixterm-git chromium cpio qpwgraph github-cli dolphin lollypop 1password 
+
