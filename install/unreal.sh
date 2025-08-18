b8aed0493fa32152dae164691399c60f4396c93c
Omar Mashaal <omar@teacups.io>
2025-08-19 08:31:15 +1000

    Create unreal.sh

---
+++ install/unreal.sh
@@ -0,0 +1,8 @@
+# haskell
+if gum confirm "Unweal Engin:3?"; then
+  git clone https://github.com/EpicGames/UnrealEngine ~/Unreal
+  cd ~/Unreal
+  sh ./Setup.sh
+  sh ./GenerateProjectFiles.sh
+  make
+fi
