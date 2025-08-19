11e5370bc70018a619af4e70ae3cc54dcb2c305a
Omar Mashaal <omar@teacups.io>
2025-08-19 18:53:33 +1000

    Update algorave.sh

--- install/algorave.sh
+++ install/algorave.sh
@@ -3,14 +3,14 @@
 set -euo pipefail
 
 if gum confirm "👾 Algorave?"; then
+  yay -S --needed --noconfirm supercollider qpwgraph
+
+  version=$(git ls-remote https://github.com/musikinformatik/SuperDirt.git | grep tags | tail -n1 | awk -F/ '{print $NF}'); echo 'Quarks.install("https://github.com/musikinformatik/SuperDirt.git", "tags/'$version'"); 0.exit;' > /tmp/sd.scd && sclang /tmp/sd.scd
+
   if ! command -v ghcup &>/dev/null; then
     curl --proto '=https' --tlsv1.2 -sSf https://get-ghcup.haskell.org | BOOTSTRAP_HASKELL_NONINTERACTIVE=1 sh
     source "$HOME/.ghcup/env"
   fi
-
-  yay -S --needed --noconfirm supercollider qpwgraph
-
-  version=$(git ls-remote https://github.com/musikinformatik/SuperDirt.git | grep tags | tail -n1 | awk -F/ '{print $NF}'); echo 'Quarks.install("https://github.com/musikinformatik/SuperDirt.git", "tags/'$version'"); 0.exit;' > /tmp/sd.scd && sclang /tmp/sd.scd
 
   if ! command -v fourmolu &>/dev/null; then
     cabal install fourmolu
