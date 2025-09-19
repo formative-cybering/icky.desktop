65a9185ca9eaee600cf8898542f857b57439f438
Omar Mashaal <omar@teacups.io>
2025-09-19 10:00:42 +0000

    blf

--- hypr/shaders/blue.glsl
+++ hypr/shaders/blue.glsl
@@ -1,14 +1,15 @@
+#version 300 es
 precision highp float;
-varying vec2 v_texcoord;
+in vec2 v_texcoord;
 uniform sampler2D tex;
-
+out vec4 FragColor;
 void main() {
-    vec4 pixColor = texture2D(tex, v_texcoord);
-    
+    vec4 pixColor = texture(tex, v_texcoord);
+   
     // Strong blue light filter: reduce blue channel and apply warm tint
     vec3 warm_tint = vec3(1.0, 0.45, 0.0); // Bias towards red/orange
     pixColor.rgb *= warm_tint;
     pixColor.b *= 0.1; // Further reduce blue intensity
-    
-    gl_FragColor = pixColor;
+   
+    FragColor = pixColor;
 }
